0b15a2801d670dfc68597787c13315be4a58513d
polansky00 <andrealipari00@gmail.com>
2025-04-15 16:00:54 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import Link from "next/link";
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>Fanta UDG con MongoDB</h1>
-      <p>
-        Vai a <Link href="/api/test">/api/test</Link> per vedere eventi dal DB
-      </p>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+  }, [router]);
+
+  return null;
 }
