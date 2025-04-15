b3bac700b7c7c59f660b286bc20cec1a6ab0e67b
polansky00 <andrealipari00@gmail.com>
2025-04-15 16:44:03 +0200

    Add files via upload

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,12 +1,9 @@
+
 import clientPromise from '../../lib/mongodb';
 
 export default async function handler(req, res) {
-  try {
-    const client = await clientPromise;
-    const db = client.db();
-    const collections = await db.listCollections().toArray();
-    res.status(200).json(collections);
-  } catch (e) {
-    res.status(500).json({ error: "Errore di connessione al database" });
-  }
+  const client = await clientPromise;
+  const db = client.db('fantaudg');
+  const events = await db.collection('events').find().toArray();
+  res.status(200).json(events);
 }
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,8 @@ import { useRouter } from 'next/router';
 
 export default function Home() {
   const router = useRouter();
-
   useEffect(() => {
     router.push('/login');
   }, [router]);
-
   return null;
 }
