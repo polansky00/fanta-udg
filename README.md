1d4f9c555971f9271c21fceb51edbb96d0c28284
polansky00 <andrealipari00@gmail.com>
2025-04-15 13:47:19 +0200

    Add files via upload
    Prima versione

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fanta UDG
+Gioco gestionale per Ufficiali di Gara del Taekwondo.
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: 40 }}>
+      <h1>Benvenuto in Fanta UDG</h1>
+      <p>La piattaforma digitale per gli Ufficiali di Gara del Taekwondo.</p>
+    </div>
+  );
+}
